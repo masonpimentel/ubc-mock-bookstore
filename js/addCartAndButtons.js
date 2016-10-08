dc8482cb1c1af06b5f1c4e48eb25da1bad10ded1
Manolito Pimentel <mpimentel947@hotmail.com>
2016-10-08 12:22:30 -0700

    Can now update products object and markup when
    clicking add or remove for each product.

--- js/addCartAndButtons.js
+++ js/addCartAndButtons.js
@@ -1,9 +1,16 @@
+/*
+ * This file applies the cartContainer template to each product
+ */
+
 var productListInit = document.getElementsByClassName("product");
 var cart = document.getElementById("cartContainer").innerHTML;
 
 var productName;
+//(for... in doesn't work because enumerable properties > length)
 for (var i=0; i<productListInit.length; i++){
     productName = productListInit[i].id;
-    cart = cart.replace("productName", productName);
+    //need to get the original template again
+    cart = document.getElementById("cartContainer").innerHTML
+    cart = cart.replace(/productName/g, productName);
     productListInit[i].innerHTML += cart;
 }
--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -4,18 +4,34 @@ var products = {};
 
 //product initialization
 var productList = document.getElementsByClassName("product");
-var productListElement = 0;
 for (var i=0; i<productList.length; i++) {
+    //create a new property in products object for each product
+    //product property is set to the quantity element
     products[productList[i].id] = productList[i].getElementsByClassName("quantity")[0].textContent;
-    productListElement++;
 }
 
 var inactiveTime = 0;
 
+function updateQuantity(productName, quantity) {
+    //TODO: question - do we need to update the markup?
+    document.getElementById(productName).getElementsByClassName("quantity").innerHTML = quantity;
+    products[productName] = quantity;
+}
+
 function addToCart(productName) {
-    window.alert("Adding " + productName + " to cart!");
+    var quantity = products[productName] - 1;
+    if (quantity == -1) {
+        window.alert("Sorry not in stock!");
+        return;
+    }
+    //update the markup and products object
+    updateQuantity(productName, quantity)
+    window.alert("Adding " + productName + " to cart! Quantity now = " + quantity);
 }
 
 function removeFromCart(productName) {
-    window.alert("Removing " + productName + " from cart!");
+    var quantity = products[productName] + 1;
+    //update the markup and products object
+    updateQuantity(productName, quantity);   
+    window.alert("Removing " + productName + " from cart! Quantity now = " + quantity);
 }
