acf236f36aeffd7aa271cbb8618402cd700b19d3
Manolito Pimentel <mpimentel947@hotmail.com>
2016-11-27 13:55:27 -0800

    Added error 400 and 404 pages

--- README.md
+++ README.md
@@ -7,19 +7,19 @@
 2. Start MongoDB: ```mongod --dbpath=<datalocation>```
   * Ex. ```mongod --dbpath=/data```
 3. Open another terminal and run ```mongo``` to start a mongo shell
-4. In the mongo shell run ```load('initdb.js')``` to initialize the DB
+4. In the mongo shell run ```load('initdb.js')``` to initialize the DB (can also do this after first setup to
+re-initialize the DB)
 5. ```cd CPEN400A-2016-L1A-Group13```
 6. ```npm install mongodb --save```
+7. ```npm install ejs```
 7. ```npm start```
 
 ### After the first time, just do the following:
 
 1. ```mongod --dbpath=<datalocation>```
   * Ex. ```mongod --dbpath=/data```
-2. Open another terminal and run ```mongo``` to start a mongo shell
-3. In the mongo shell run ```load('initdb.js')``` to initialize the DB
-4. ```cd CPEN400A-2016-L1A-Group13```
-5. ```npm start```
+2. ```cd CPEN400A-2016-L1A-Group13```
+3. ```npm start```
 
 Can now go to [localhost:5000](http://localhost:5000/) to access locally
 Hosted on Heroku at: https://cpen400group13.herokuapp.com/
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var MAX_VALUE = 1000;
+
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
+var fs = require('fs');
 
 //set up express node app
 app.set('port', (process.env.PORT || 5000));
@@ -8,12 +12,27 @@ app.use(express.static(__dirname + '/public'));
 //require mongoDb.js
 var mongodb = require("./mongoDb.js");
 
-app.get('/', function(request, response) {
-    response.sendFile("index.html");
-});
-
 app.get('/products', function(request, response) {
     mongodb.getProducts(response);
+});
+
+app.get('/products/range/:min-:max', function(request, response) {
+    var min = request.params['min'];
+    var max = request.params['max'];
+    var renderedHtml;
+    fs.readFile("public/range_error.html", 'utf-8', function(err, content) {
+        if (err) {
+            throw ("EJS error");
+        }
+        renderedHtml = ejs.render(content, {max: MAX_VALUE});
+        if (isNaN(min) || isNaN(max) || (min < 0) || (max > MAX_VALUE)) {
+            response.status(400).send(renderedHtml);
+        }
+    });
+});
+
+app.get('/*', function(request, response) {
+    response.status(404).sendFile(__dirname + "/public/not_found_error.html");
 });
 
 app.listen(app.get('port'), function() {
