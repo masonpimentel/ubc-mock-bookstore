37cae9d0d0402dfdb67acb00a237a57d0b55f615
Manolito Mason Pimentel <mpimentel947@hotmail.com>
2018-01-15 20:06:25 -0800

    Favicon and timeout removed properly

--- public/js/timeout.js
+++ public/js/timeout.js
@@ -10,29 +10,30 @@ var inactiveTime = 0;
  * Color is changed to yellow when half time is up
  * Color is changed to red when quarter time remains
  */
-function incrementInactiveTime() {
-    inactiveTime++;
-    //update the timer in the footer
-    var diff = String(TIMEOUT_SEC - inactiveTime);
-    document.getElementById("timerVal").innerHTML = diff;
-    if (diff/TIMEOUT_SEC < .25) {
-        document.getElementById("timerVal").style.color = "red";
-    }
-    else if (diff/TIMEOUT_SEC < .5) {
-        document.getElementById("timerVal").style.color = "yellow";
-    }
-    else {
-        document.getElementById("timerVal").style.color = "green";
-    }
-    if (inactiveTime == TIMEOUT_SEC) {
-        resetTimer();
-        window.alert("Hey there! Are you still planning to buy something?");
-        document.getElementById("timerVal").style.color = "green";
-        document.getElementById("timerVal").innerHTML = String(TIMEOUT_SEC);
-    }
-}
-
+// function incrementInactiveTime() {
+//     inactiveTime++;
+//     //update the timer in the footer
+//     var diff = String(TIMEOUT_SEC - inactiveTime);
+//     document.getElementById("timerVal").innerHTML = diff;
+//     if (diff/TIMEOUT_SEC < .25) {
+//         document.getElementById("timerVal").style.color = "red";
+//     }
+//     else if (diff/TIMEOUT_SEC < .5) {
+//         document.getElementById("timerVal").style.color = "yellow";
+//     }
+//     else {
+//         document.getElementById("timerVal").style.color = "green";
+//     }
+//     if (inactiveTime == TIMEOUT_SEC) {
+//         resetTimer();
+//         window.alert("Hey there! Are you still planning to buy something?");
+//         document.getElementById("timerVal").style.color = "green";
+//         document.getElementById("timerVal").innerHTML = String(TIMEOUT_SEC);
+//     }
+// }
+//
 function resetTimer() {
+    return;
     inactiveTime = 0;
     clearInterval(interval);
     document.getElementById("timerVal").style.color = "green";
